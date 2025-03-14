f44e3e133bfa8b54397b6b4c0638a2a6d6fbbcc4
DEVELOPPEUR JAVA <developpeuralvaro@gmail.com>
2025-03-14 05:23:57 +0100

    first commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'cm.kapexpert.fastgaz',
+  appName: 'FastGaz',
+  webDir: 'www'
+};
+
+export default config;
