dc93a176ee0909eb06fe247b249ede4b5f672c09
DEVELOPPEUR JAVA <developpeuralvaro@gmail.com>
2025-03-14 10:35:00 +0100

    final commit phase 1

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,11 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'cm.kapexpert.fastgaz',
   appName: 'FastGaz',
-  webDir: 'www'
+  webDir: 'www',
+  server: {
+    url: 'https://kapexpert.cloud:30004/', // Remplace par l'URL de ton frontend hébergé
+    cleartext: true // Autorise HTTP (utile en dev, sinon mettre `false` en prod)
+  },
 };
 
 export default config;
